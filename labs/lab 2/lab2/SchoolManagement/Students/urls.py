6316aec5a2701fcbb63b563e461409a805e6cb64
Saleh AL-gabri <s96557867@gmail.com>
2024-10-06 04:21:34 +0300

    some changes in lab 2

--- labs/lab 2/lab2/SchoolManagement/Students/urls.py
+++ labs/lab 2/lab2/SchoolManagement/Students/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns =[
     path('',views.index,name="index"),
-    path('home/',views.home,name="home"),
+    path('home',views.home,name="home"),
     path('showstudents',views.showstudents,name="show"),
     path('editstudents',views.editstudents,name="edit"),
     path('deletestudents',views.deletestudents,name="delete"),
