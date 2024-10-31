7941b1e2a66a5ba6b8b05cf7013e973591610e43
Saleh AL-gabri <s96557867@gmail.com>
2024-10-31 17:24:28 +0300

    remove quiz

--- Assignment/quiz/School_Management_System/Home/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render,redirect
-from django.http import HttpResponse
-from django.urls import reverse
-
-# Create your views here.
-def Home(request):
-    return render(request,'Home/Home.html')
-
--- Assignment/quiz/School_Management_System/Teachers/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def Teachers(request):
-    return HttpResponse("Hello world")
