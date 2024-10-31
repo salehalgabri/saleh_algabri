fbb6c0d8558f0b67a3c37d48c0b9676b35ae4240
Saleh AL-gabri <s96557867@gmail.com>
2024-10-31 17:14:53 +0300

    adding lab 5

---
+++ labs/lab 5/patients/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+app_name = 'patients'  # هذا السطر يعرّف الـ namespace الخاص بالتطبيق
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    # path('create/', views.patients_create, name='create'),
+    # path('show/', views.patients_list, name='show'),
+    # path('success/', views.message_success, name='message_success'),
+    # path('error/', views.error_page, name='error_page'),
+    # path('show/<int:pk>/', views.patients_show_detail, name='showdetail'),  # تأكد من تصحيح اسم الدالة
+    # path('update/<int:pk>/', views.patients_edit, name='edit'),
+    # path('delete/<int:pk>/', views.patients_delete, name='delete'),
+    # path('forms/', views.show_forms, name='forms'),
+]
